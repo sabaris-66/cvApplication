fda48f857eeadf2dafe5cd98f246c806b538b728
Sabaris <sabaris.elango@gmail.com>
2024-10-21 05:12:44 +0530

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { useState } from "react";
+
+createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
