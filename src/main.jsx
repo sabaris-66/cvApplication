8aa814362e7015dce03d74588ca0ad7b17eaef53
Sabaris <sabaris.elango@gmail.com>
2024-10-21 06:27:20 +0530

    Add HomePage component to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { useState } from "react";
+import HomePage from "./components/homePage";
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <HomePage></HomePage>
+  </StrictMode>
+);
