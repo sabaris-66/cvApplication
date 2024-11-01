6ed1529da328798839d4f6ecb369a98960187e4a
Sabaris <sabaris.elango@gmail.com>
2024-11-01 19:45:06 +0530

    Add import lines to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import HomePage from "./components/homePage";
+import "./styles/homePageStyles.css";
+import "./styles/cvPageStyles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
